fab4e2c4d9d90fa3622e85cca6b9e8cc4837d104
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-03-14 23:30:58 -0700

    Release v0.0.7

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Filter and order an enumerable collection of structs
 
 ```elixir
 def deps do
-  [{:enumerati, "~> 0.0.6"}]
+  [{:enumerati, "~> 0.0.7"}]
 end
 ```
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Enumerati.MixProject do
   def project do
     [
       app: :enumerati,
-      version: "0.0.6",
+      version: "0.0.7",
       elixir: "~> 1.7",
       package: package(),
       description: description(),
